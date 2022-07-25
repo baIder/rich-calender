23e9f79deee74f3b3e740ac76ffc05c431483eab
baIder <511763880@qq.com>
2022-07-25 17:17:29 +0800

    重构代码

--- main.js
+++ main.js
@@ -1,43 +1,48 @@
-const g = selector => {
-  return document.querySelector(selector)
-}
-
-const gs = selector => {
-  return document.querySelectorAll(selector)
-}
-
-const time = g('#time')
-const days = g('#days')
-
 const now = new Date(2022, 8, 1)
 const year = now.getFullYear()
 const month = now.getMonth() + 1
 
-time.textContent = `${year}年${month}月`
+initialTime()
 
-const firstDayOfCurrentMonth = new Date(year, month - 1, 1)
-const weekdayOfFirstDayOfCurrentMonth = firstDayOfCurrentMonth.getDay()
-const lastDayOfCurrentMonth = new Date(new Date(year, month - 1 + 1, 1) - 86400 * 1000)
+generateDays(year, month)
 
-const daysOfCurrentMonth = lastDayOfCurrentMonth.getDate()
-
-for (let i = 1; i < weekdayOfFirstDayOfCurrentMonth; i++) {
-  const li = document.createElement('li')
-  const d = new Date(firstDayOfCurrentMonth - 86400 * 1000 * i)
-  li.textContent = d.getDate().toString()
-  days.prepend(li)
+function initialTime() {
+  const time = g('#time')
+  time.textContent = `${year}年${month}月`
 }
 
-for (let i = 1; i <= daysOfCurrentMonth; i++) {
-  const li = document.createElement('li')
-  li.textContent = i.toString()
-  days.append(li)
+function generateDays(year, month) {
+  const days = g('#days')
+  const firstDayOfCurrentMonth = new Date(year, month - 1, 1)
+  const weekdayOfFirstDayOfCurrentMonth = firstDayOfCurrentMonth.getDay()
+  const lastDayOfCurrentMonth = new Date(new Date(year, month - 1 + 1, 1) - 86400 * 1000)
+
+  const daysOfCurrentMonth = lastDayOfCurrentMonth.getDate()
+
+  for (let i = 1; i < weekdayOfFirstDayOfCurrentMonth; i++) {
+    const li = document.createElement('li')
+    const d = new Date(firstDayOfCurrentMonth - 86400 * 1000 * i)
+    li.textContent = d.getDate().toString()
+    days.prepend(li)
+  }
+
+  for (let i = 1; i <= daysOfCurrentMonth; i++) {
+    const li = document.createElement('li')
+    li.textContent = i.toString()
+    days.append(li)
+  }
+
+  for (let i = lastDayOfCurrentMonth.getDay() + 1; i <= 7; i++) {
+    const li = document.createElement('li')
+    li.textContent = (i - lastDayOfCurrentMonth.getDay()).toString()
+    days.append(li)
+  }
 }
 
-console.log(lastDayOfCurrentMonth.getDay())
+function g(selector) {
+  return document.querySelector(selector)
+}
 
-for (let i = lastDayOfCurrentMonth.getDay() + 1; i <= 7; i++) {
-  const li = document.createElement('li')
-  li.textContent = (i - lastDayOfCurrentMonth.getDay()).toString()
-  days.append(li)
+function gs(selector) {
+  return document.querySelectorAll(selector)
 }
